10ca987c1585918e90105ba08b07c717978f9a0a
adithya-s-sekhar <60822601+adithya-s-sekhar@users.noreply.github.com>
2021-04-18 19:43:07 +0530

    v2.2 source from sourceforge

--- AYClient.bat
+++ AYClient.bat
@@ -7,14 +7,17 @@ set dlmode=
 set stream=
 set qual=
 set fmt=
+set gpq=
 :start
 cls
 echo.
 echo --------------------------------------------------------------------------------
-echo                          Advanced Youtube Client - AYC v2.1
+echo                          Advanced Youtube Client - AYC v2.2
 echo --------------------------------------------------------------------------------
 echo.
+echo ------------------------------------------------------
 echo  Downloaded files are saved in your 'My Videos' folder
+echo ------------------------------------------------------
 echo.
 set /p url=Paste a Youtube Video/Playlist URL to start: 
 if "%url%" equ "" goto start
@@ -22,15 +25,18 @@ if "%url%" equ "" goto start
 cls
 echo.
 echo --------------------------------------------------------------------------------
-echo                          Advanced Youtube Client - AYC v2.1
+echo                          Advanced Youtube Client - AYC v2.2
 echo --------------------------------------------------------------------------------
 echo.
 echo -------------
 echo  Choose mode
 echo -------------
+echo.
 echo  1) Download Video+Audio
+echo.
 echo  2) Download Audio Only
 echo.
+echo --------------------------
 set /p dlmode=Enter Download mode (1-2): 
 if "%dlmode%" equ "" goto menu
 if %dlmode% == 1 goto video
@@ -41,7 +47,7 @@ if %dlmode% LSS 1 goto menu
 cls
 echo.
 echo --------------------------------------------------------------------------------
-echo                          Advanced Youtube Client - AYC v2.1
+echo                          Advanced Youtube Client - AYC v2.2
 echo --------------------------------------------------------------------------------
 echo.
 echo ---------------------------------
@@ -49,9 +55,12 @@ echo  Choose which Stream to Download
 echo ---------------------------------
 echo.
 echo  1) MP4 - H264 (Default)
+echo.
 echo  2) WEBM - VP9 (Smaller but less compatible)
+echo.
 echo  3) 3GP - MPEG 4 (Compatible for old phones)
 echo.
+echo --------------------------
 set /p stream=Enter Option Number (1-3): 
 if "%stream%" equ "" goto video
 if %stream% == 1 goto mp4
@@ -63,19 +72,23 @@ if %stream% LSS 1 goto video
 cls
 echo.
 echo --------------------------------------------------------------------------------
-echo                          Advanced Youtube Client - AYC v2.1
+echo                          Advanced Youtube Client - AYC v2.2
 echo --------------------------------------------------------------------------------
 echo.
 echo ----------------
 echo  Choose Quality
 echo ----------------
+echo.
 echo  1) 360p 
 echo  2) 480p (If not available, returns to 360p)
+echo ---------------------------------------------
 echo  3) 720p (If not available, returns to 480p)
 echo  4) 1080p (If not available, returns to 720p)
+echo ---------------------------------------------
 echo  5) 2K (If not available, returns to 1080p)
 echo  6) 4K (If not available, returns to 2K)
 echo.
+echo --------------------
 set /p qual=Enter Choice (1-6): 
 if "%qual%" equ "" goto mp4
 if %qual% == 1 set qual="bestvideo[ext=mp4][height<=360]+bestaudio[ext=m4a]"
@@ -91,19 +104,23 @@ goto download
 cls
 echo.
 echo --------------------------------------------------------------------------------
-echo                          Advanced Youtube Client - AYC v2.1
+echo                          Advanced Youtube Client - AYC v2.2
 echo --------------------------------------------------------------------------------
 echo.
 echo ----------------
 echo  Choose Quality
 echo ----------------
+echo.
 echo  1) 360p 
 echo  2) 480p (If not available, returns to 360p)
+echo ---------------------------------------------
 echo  3) 720p (If not available, returns to 480p)
 echo  4) 1080p (If not available, returns to 720p)
+echo ---------------------------------------------
 echo  5) 2K (If not available, returns to 1080p)
 echo  6) 4K (If not available, returns to 2K)
 echo.
+echo --------------------
 set /p qual=Enter Choice (1-6): 
 if "%qual%" equ "" goto vp9
 if %qual% == 1 set qual="bestvideo[ext=webm][height<=360]+bestaudio[ext=webm]"
@@ -119,33 +136,41 @@ goto download
 cls
 echo.
 echo --------------------------------------------------------------------------------
-echo                          Advanced Youtube Client - AYC v2.1
+echo                          Advanced Youtube Client - AYC v2.2
 echo --------------------------------------------------------------------------------
 echo.
 echo ----------------
 echo  Choose Quality
 echo ----------------
+echo.
 echo  1) 240p (Recommended)
+echo.
 echo  2) 144p
 echo.
-set /p qual=Enter Choice (1-2): 
-if "%qual%" equ "" goto 3gp
-if %qual% == 1 set qual=36
-if %qual% == 2 set qual=17
+echo -------------------------
+set /p gpq=Enter Choice (1-2): 
+if "%gpq%" equ "" goto 3gp
+if %gpq% == 1 set qual=36
+if %gpq% == 2 set qual=17
+if %gpq% GTR 2 goto 3gp
+if %gpq% LSS 1 goto 3gp
 goto download
 :audio
 cls
 echo.
 echo --------------------------------------------------------------------------------
-echo                          Advanced Youtube Client - AYC v2.1
+echo                          Advanced Youtube Client - AYC v2.2
 echo --------------------------------------------------------------------------------
 echo.
 echo --------------
 echo  Choose Format
 echo --------------
 echo.
-echo  1) MP3 (External)
+echo  1) MP3
+echo.
 echo  2) M4A
+echo.
+echo -------------------
 set /p fmt=Enter Choice (1-2): 
 if "%fmt%" equ "" goto audio
 if %fmt% == 1 goto mp3
@@ -157,61 +182,93 @@ goto audiodownload
 cls
 echo.
 echo --------------------------------------------------------------------------------
-echo                          Advanced Youtube Client - AYC v2.1
+echo                          Advanced Youtube Client - AYC v2.2
 echo --------------------------------------------------------------------------------
 echo.
 echo -------------------
 echo  Starting Download
 echo -------------------
+echo.
 youtube-dl --ignore-errors -f %qual% --external-downloader aria2c --external-downloader-args "-x 8 -s 8 -k 1M" -o "%loc%\%%(title)s-%%(height)sp.%%(ext)s" "%url%"
 cls
 echo.
 echo --------------------------------------------------------------------------------
-echo                          Advanced Youtube Client - AYC v2.1
+echo                          Advanced Youtube Client - AYC v2.2
 echo --------------------------------------------------------------------------------
 echo.
-echo  Your download is Finished, The files are saved in Your Videos Folder
+echo  Your download is Finished, The files are saved in Your 'My Videos' Folder.
+echo.
+echo  Press ENTER to read a message or You can just close the program.
 pause>NUL
 goto exit
 :audiodownload
 cls
 echo.
 echo --------------------------------------------------------------------------------
-echo                          Advanced Youtube Client - AYC v2.1
+echo                          Advanced Youtube Client - AYC v2.2
 echo --------------------------------------------------------------------------------
 echo.
 echo -------------------
 echo  Starting Download
 echo -------------------
+echo.
 youtube-dl --ignore-errors -f bestaudio[ext=m4a] --external-downloader aria2c --external-downloader-args "-x 8 -s 8 -k 1M" -o "%loc%\%%(title)s.%%(ext)s" "%url%"
 cls
 echo.
 echo --------------------------------------------------------------------------------
-echo                          Advanced Youtube Client - AYC v2.1
+echo                          Advanced Youtube Client - AYC v2.2
 echo --------------------------------------------------------------------------------
 echo.
-echo  Your download is Finished, The files are saved in Your Videos Folder
+echo  Your download is Finished, The audio(s) are saved in Your 'My Videos' Folder.
+echo.
+echo  Press ENTER to read a message or You can just close the program.
 pause>NUL
 goto exit
 :mp3
 cls
 echo.
 echo --------------------------------------------------------------------------------
-echo                          Advanced Youtube Client - AYC v2.1
+echo                          Advanced Youtube Client - AYC v2.2
 echo --------------------------------------------------------------------------------
 echo.
 echo -------------------
 echo  Starting Download
 echo -------------------
 echo.
-youtube-dl --audio-format mp3 --audio-quality 128k --embed-thumbnail --restrict-filenames --ignore-errors --external-downloader aria2c --external-downloader-args "-x 8 -s 8 -k 1M" -o "%loc%\%%(title)s.%%(ext)s" "%url%"
+youtube-dl --extract-audio --audio-format mp3 --audio-quality 128k --embed-thumbnail --ignore-errors --external-downloader aria2c --external-downloader-args "-x 8 -s 8 -k 1M" -o "%loc%\%%(title)s.%%(ext)s" "%url%"
 cls
 echo.
 echo --------------------------------------------------------------------------------
-echo                          Advanced Youtube Client - AYC v2.1
+echo                          Advanced Youtube Client - AYC v2.2
 echo --------------------------------------------------------------------------------
 echo.
-echo  Your download has finished, The files are saved in Your Videos Folder
+echo  Your download is finished, The song(s) are saved in Your 'My Videos' Folder.
+echo.
+echo  Press ENTER to read a message or You can just close the program.
 pause>NUL
 goto exit
 :exit
+cls
+echo.
+echo --------------------------------------------------------------------------------
+echo                          Advanced Youtube Client - AYC v2.2
+echo --------------------------------------------------------------------------------
+echo.
+echo -------------------------------------
+echo  JUST A MESSAGE FROM THE DEVELOPER
+echo  You may skip this if you are not interested by pressing enter.
+echo.
+echo  Thank you for using Advanced Youtube Client - AYC.
+echo.
+echo  This is a simple Open Source Project made to simplify the process of 
+echo  downloading Youtube Videos. But I do understand it is a bit alienating for 
+echo  people, despite my best efforts to be as user friendly as possible because 
+echo  of this console style look.
+echo.
+echo  The thing is I don't know how to make a GUI (yet), but I am trying to learn
+echo  and hopefully if things go well, I will come up with something quite soon.
+echo.
+echo ----------------------------------------------
+echo  In the mean time, you can report various bugs or request new features
+echo  from the program on our Sourceforge page.
+pause>NUL
