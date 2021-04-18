89832a1bf778eb9cbac16eb5c57db14f168031f5
Adithya S Sekhar <adithya0332@gmail.com>
2021-04-18 20:18:52 +0530

    v2.45.5 source from sourceforge

--- AYClient.bat
+++ AYClient.bat
@@ -1,7 +1,7 @@
 @echo off
 mode con:cols=80 lines=30
-set version=v2.45.4 (04/Nov/2017)
-title Advanced Youtube Client - AYC v2.45.4
+set version=v2.45.5 (14/Dec/2017)
+title Advanced Youtube Client - AYC v2.45.5
 md "%userprofile%\Videos\Advanced Youtube Client - AYC"
 set loc=%userprofile%\Videos\Advanced Youtube Client - AYC
 :start
