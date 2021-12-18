644940d50ac42655d67753a86134e0cf0c7a0d4c
Adithya S Sekhar <adithya0332@gmail.com>
2021-12-18 17:37:55 +0530

    all: append video id to end of file name

--- AYClient.bat
+++ AYClient.bat
@@ -319,10 +319,10 @@ echo -------------------
 echo.
 echo  URL: %url%
 echo.
-if %format_chosen% == h264 %youtube_dl% --ignore-errors --no-warnings %conf% --external-downloader aria2c -o "%loc%\%%(title)s-MP4-%%(height)sp.%%(ext)s" "%url%" && goto downloadsuccess
-if %format_chosen% == vp9 %youtube_dl% --ignore-errors --no-warnings %conf% --external-downloader aria2c -o "%loc%\%%(title)s-VP9-%%(height)sp.%%(ext)s" "%url%" && goto downloadsuccess
-if %format_chosen% == av1 %youtube_dl% --ignore-errors --no-warnings %conf% --external-downloader aria2c -o "%loc%\%%(title)s-AV1-%%(height)sp.%%(ext)s" "%url%" && goto downloadsuccess
-if %format_chosen% == aud %youtube_dl% --ignore-errors --no-warnings %conf% --external-downloader aria2c -o "%loc%\%%(title)s.%%(ext)s" "%url%" && goto downloadsuccess
+if %format_chosen% == h264 %youtube_dl% --ignore-errors --no-warnings %conf% --external-downloader aria2c -o "%loc%\%%(title)s-MP4-%%(height)sp-%%(id)s.%%(ext)s" "%url%" && goto downloadsuccess
+if %format_chosen% == vp9 %youtube_dl% --ignore-errors --no-warnings %conf% --external-downloader aria2c -o "%loc%\%%(title)s-VP9-%%(height)sp-%%(id)s.%%(ext)s" "%url%" && goto downloadsuccess
+if %format_chosen% == av1 %youtube_dl% --ignore-errors --no-warnings %conf% --external-downloader aria2c -o "%loc%\%%(title)s-AV1-%%(height)sp-%%(id)s.%%(ext)s" "%url%" && goto downloadsuccess
+if %format_chosen% == aud %youtube_dl% --ignore-errors --no-warnings %conf% --external-downloader aria2c -o "%loc%\%%(title)s-%%(id)s.%%(ext)s" "%url%" && goto downloadsuccess
 set /a try=%try%+1
 if %try% GTR %defined_try% goto error
 goto downloadtried
@@ -865,10 +865,10 @@ echo.
 echo  Starting Download
 echo -------------------
 echo.
-if %format_chosen% == h264 %youtube_dl% --ignore-errors --no-warnings %conf% --external-downloader aria2c -o "%loc%\%job_name%\%%(title)s-MP4-%%(height)sp.%%(ext)s" -a "%loc%\%job_name%\%job_name%.txt" && goto batch_downloadsuccess
-if %format_chosen% == vp9 %youtube_dl% --ignore-errors --no-warnings %conf% --external-downloader aria2c -o "%loc%\%job_name%\%%(title)s-VP9-%%(height)sp.%%(ext)s" -a "%loc%\%job_name%\%job_name%.txt" && goto batch_downloadsuccess
-if %format_chosen% == av1 %youtube_dl% --ignore-errors --no-warnings %conf% --external-downloader aria2c -o "%loc%\%job_name%\%%(title)s-AV1-%%(height)sp.%%(ext)s" -a "%loc%\%job_name%\%job_name%.txt" && goto batch_downloadsuccess
-if %format_chosen% == aud %youtube_dl% --ignore-errors --no-warnings %conf% --external-downloader aria2c -o "%loc%\%job_name%\%%(title)s.%%(ext)s" -a "%loc%\%job_name%\%job_name%.txt" && goto batch_downloadsuccess
+if %format_chosen% == h264 %youtube_dl% --ignore-errors --no-warnings %conf% --external-downloader aria2c -o "%loc%\%job_name%\%%(title)s-MP4-%%(height)sp-%%(id)s.%%(ext)s" -a "%loc%\%job_name%\%job_name%.txt" && goto batch_downloadsuccess
+if %format_chosen% == vp9 %youtube_dl% --ignore-errors --no-warnings %conf% --external-downloader aria2c -o "%loc%\%job_name%\%%(title)s-VP9-%%(height)sp-%%(id)s.%%(ext)s" -a "%loc%\%job_name%\%job_name%.txt" && goto batch_downloadsuccess
+if %format_chosen% == av1 %youtube_dl% --ignore-errors --no-warnings %conf% --external-downloader aria2c -o "%loc%\%job_name%\%%(title)s-AV1-%%(height)sp-%%(id)s.%%(ext)s" -a "%loc%\%job_name%\%job_name%.txt" && goto batch_downloadsuccess
+if %format_chosen% == aud %youtube_dl% --ignore-errors --no-warnings %conf% --external-downloader aria2c -o "%loc%\%job_name%\%%(title)s-%%(id)s.%%(ext)s" -a "%loc%\%job_name%\%job_name%.txt" && goto batch_downloadsuccess
 if %format_chosen% == batch %youtube_dl% --ignore-errors --no-warnings %conf% --external-downloader aria2c -o "%loc%\%job_name%\%%(title)s-%batch_name_end%-%%(id)s.%%(ext)s" -a "%loc%\%job_name%\%job_name%.txt" && goto batch_downloadsuccess
 set /a try=%try%+1
 if %try% GTR %defined_try% goto batch_error
