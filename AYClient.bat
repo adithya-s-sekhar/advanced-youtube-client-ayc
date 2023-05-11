1c3ccc9f283c1dad6ee451d08f5d4e0aee218685
Adithya S Sekhar <adithya0332@gmail.com>
2023-05-12 00:43:00 +0530

    batch: don't continue if job name is empty

--- AYClient.bat
+++ AYClient.bat
@@ -549,6 +549,7 @@ echo.
 echo ------------------------------------------
 echo.
 set /p job_name=Enter Job Name (eg: Songs): 
+if "%job_name%" equ "" goto batch
 md "%loc%\%job_name%"
 if exist "%loc%\%job_name%\%job_name%.txt" set batch_exists_true=1 && goto batchIsYoutubeCheck
 echo.>"%loc%\%job_name%\%job_name%.txt"
