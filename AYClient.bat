227c6e0d55268e640cbae41a7ae84eaafe36aea0
Adithya S Sekhar <adithya0332@gmail.com>
2023-06-10 08:55:56 +0530

    refactor: ayclient -> about

--- AYClient.bat
+++ AYClient.bat
@@ -304,7 +304,7 @@ choice /c 012345 /n /m "Enter Choice (0-5): "
 if %errorlevel% == 1 goto start
 if %errorlevel% == 2 start AYClient.bat "b" && goto more
 if %errorlevel% == 3 call settingsMenu
-if %errorlevel% == 4 goto about
+if %errorlevel% == 4 call about
 if %errorlevel% == 5 start "" "https://github.com/adithya-s-sekhar/advanced-youtube-client-ayc"
 if %errorlevel% == 6 start "" "https://sourceforge.net/projects/advanced-youtube-client-ayc"
 goto more
@@ -770,30 +770,6 @@ pause>NUL
 if %error_mode% == batch goto batch
 if %error_mode% == uni exit
 if %error_mode% == regular goto exit
-
-
-:about
-mode %window_small%
-color 07
-title You're a curious one..
-cls
-call gui bannerSmall
-echo.
-echo   Advanced Youtube Client - AYC Script
-echo.
-echo   Author           : Adithya S Sekhar
-echo.
-echo   First Release    : v1.0 (13/Aug/2016)
-echo.
-echo   Current Release  : %version%
-echo.
-echo   Made with (love) in Kerala.
-echo.
-echo   Released under the MIT License.
-echo.
-echo   Press Enter to go back.
-pause>NUL
-goto more
 
 
 :exit
---
+++ scripts/about.bat
@@ -0,0 +1,22 @@
+:about
+mode %window_small%
+color 07
+title You're a curious one..
+cls
+call gui bannerSmall
+echo.
+echo   Advanced Youtube Client - AYC Script
+echo.
+echo   Author           : Adithya S Sekhar
+echo.
+echo   First Release    : v1.0 (13/Aug/2016)
+echo.
+echo   Current Release  : %version%
+echo.
+echo   Made with (love) in Kerala.
+echo.
+echo   Released under the MIT License.
+echo.
+echo   Press Enter to go back.
+pause>NUL
+goto :EOF
