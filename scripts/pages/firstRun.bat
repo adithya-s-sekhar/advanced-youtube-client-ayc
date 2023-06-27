df690e6ad68be5e0def7d0acf030c1fdb9c89d84
Adithya S Sekhar <adithya0332@gmail.com>
2023-06-27 12:59:13 +0530

    firstRun: simplify

--- scripts/pages/firstRun.bat
+++ scripts/pages/firstRun.bat
@@ -5,25 +5,32 @@ title Welcome to AYC
 cls
 call tui bannerMedium
 echo.
-echo  WARNING!!!
+echo  Before you start we need to take care of a few things. 
 echo.
-echo  Please follow the instructions at GitHub first on how to properly install AYC. 
+echo  Please follow the instructions on GitHub first on how to properly install AYC. 
 echo.
 echo  Press Enter to open instructions.
 pause>NUL
 start "" "https://github.com/adithya-s-sekhar/advanced-youtube-client-ayc#instructions"
 echo.
-echo  After setting up, enter the magic phrase below.
+echo  After setting up, paste the magic phrase below.
 echo.
 echo  This is to ensure you read the instructions and followed it through.
 echo.
 :firstRun2
 set "magic_phrase_input="
-set /p magic_phrase_input=Enter magic phrase: 
+set /p magic_phrase_input=Paste magic phrase: 
 set magic_phrase_input=%magic_phrase_input: =%
 if "%magic_phrase_input%" equ "" goto firstRun2
 if "%magic_phrase_input%" equ " =" goto firstRun2
-call :magic_verify
+if not "%magic_phrase_input%" == "Maurisegestasimperdietseminimperdiet" (
+    echo.
+    echo  Invalid magic phrase. Please Try again.
+    echo.
+    echo  Press Enter to cotinue.
+    pause>NUL
+    goto firstRun
+)
 echo.
 echo  Preparing for first run..
 echo.
@@ -32,28 +39,7 @@ echo  Please wait, updating yt-dlp..
 %youtube_dl% --version>"%aycdata%\youtube_dl_version.txt"
 set /p youtube_dl_version=<"%aycdata%\youtube_dl_version.txt"
 set youtube_dl_version=%youtube_dl_version:"=%
-call :magic_completed
-exit
-
-
-:magic_verify
-set magic_passed=1
-if not "%magic_phrase_input%" == "Maurisegestasimperdietseminimperdiet" set magic_passed=0
-if %magic_passed% == 0 (
-    echo.
-    echo  Invalid magic phrase. Please Try again
-    echo.
-    echo  Press Enter to exit.
-    pause>NUL
-    exit
-)
-goto :EOF
-
-
-:magic_completed
-if not %magic_passed% == 0 (
-    echo "0">"%aycdata%\first_run.txt"
-)
+echo "0">"%aycdata%\first_run.txt"
 echo.
 echo  Hope you read the instructions.
 echo.
@@ -61,4 +47,4 @@ echo  First run finished.
 echo.
 echo  You can restart AYC now. Press Enter to close.
 pause>NUL
-goto :EOF
+exit
