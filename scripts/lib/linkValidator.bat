d3b799bdf1b899a5a7efe087d24b9d9b771a8924
Adithya S Sekhar <adithya0332@gmail.com>
2023-07-02 23:33:22 +0530

    linkValidator: fix url leaking into console

--- scripts/lib/linkValidator.bat
+++ scripts/lib/linkValidator.bat
@@ -25,42 +25,42 @@ if %errorlevel% == 0 (
 
 
 :isYtLinkChecker
-echo %linkValidator.arg1% | findstr /i /r /c:"^https://www.youtube.com"
+echo %linkValidator.arg1% | findstr /i /r /c:"^https://www.youtube.com" > nul
 if %errorlevel% == 0 (
     set youtube_link=1
     goto :EOF
 )
-echo %linkValidator.arg1% | findstr /i /r /c:"^https://m.youtube.com"
+echo %linkValidator.arg1% | findstr /i /r /c:"^https://m.youtube.com" > nul
 if %errorlevel% == 0 (
     set youtube_link=1
     goto :EOF
 )
-echo %linkValidator.arg1% | findstr /i /r /c:"^https://youtube.com"
+echo %linkValidator.arg1% | findstr /i /r /c:"^https://youtube.com" > nul
 if %errorlevel% == 0 (
     set youtube_link=1
     goto :EOF
 )
-echo %linkValidator.arg1% | findstr /i /r /c:"^https://youtu.be"
+echo %linkValidator.arg1% | findstr /i /r /c:"^https://youtu.be" > nul
 if %errorlevel% == 0 (
     set youtube_link=1
     goto :EOF
 )
-echo %linkValidator.arg1% | findstr /i /r /c:"^http://www.youtube.com"
+echo %linkValidator.arg1% | findstr /i /r /c:"^http://www.youtube.com" > nul
 if %errorlevel% == 0 (
     set youtube_link=1
     goto :EOF
 )
-echo %linkValidator.arg1% | findstr /i /r /c:"^http://m.youtube.com"
+echo %linkValidator.arg1% | findstr /i /r /c:"^http://m.youtube.com" > nul
 if %errorlevel% == 0 (
     set youtube_link=1
     goto :EOF
 )
-echo %linkValidator.arg1% | findstr /i /r /c:"^http://youtube.com"
+echo %linkValidator.arg1% | findstr /i /r /c:"^http://youtube.com" > nul
 if %errorlevel% == 0 (
     set youtube_link=1
     goto :EOF
 )
-echo %linkValidator.arg1% | findstr /i /r /c:"^http://youtu.be"
+echo %linkValidator.arg1% | findstr /i /r /c:"^http://youtu.be" > nul
 if %errorlevel% == 0 (
     set youtube_link=1
     goto :EOF
