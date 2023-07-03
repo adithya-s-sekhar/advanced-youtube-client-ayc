5f8836fbde062f7233f98b65e48cf0ad531d03cc
Adithya S Sekhar <adithya0332@gmail.com>
2023-07-03 08:45:13 +0530

    downloadSuccess: display url

--- scripts/pages/downloadSuccess.bat
+++ scripts/pages/downloadSuccess.bat
@@ -5,7 +5,10 @@ title Download Finished
 cls
 call tui bannerSmall
 echo.
-echo  Download Finished, The files are saved in:
+echo  Download Finished.
+echo  URL: %url%
+echo.
+echo  The files are saved in:
 echo  %loc%
 echo.
 echo  Press enter to close this window.
