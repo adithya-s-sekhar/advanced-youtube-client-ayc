44f2c3899257d72d36f3223af4ca11b1c80c154e
Adithya S Sekhar <adithya0332@gmail.com>
2023-07-03 09:56:20 +0530

    settingsChangeDefinesTry: update ui

--- scripts/settings/settingsChangeDefinedTry.bat
+++ scripts/settings/settingsChangeDefinedTry.bat
@@ -6,29 +6,31 @@ cls
 set "settings_try="
 call tui bannerSmall
 echo.
-echo  Change retry attempts
+if %try_invalid% == 0 echo  Current retry attempts: %defined_try%
+if %try_invalid% == 1 echo  Invalid value: %defined_try%
 echo.
-echo  The number you set here is the number of times AYC will 
-echo  retry the download if a network error occurs.
+echo ----------------------------------------------
 echo.
-if %try_invalid% == 1 (
-    echo  Invalid value detected: %defined_try%
-    echo. 
-    echo  Enter a number or Enter R to reset to default value.
-)
+echo  * The number you set here is the number of times AYC
+echo    will retry the download if a network error occurs.
+echo.
 if %try_invalid% == 0 (
-    echo  Current value: %defined_try%
+    echo  * Leave blank and Enter to Go back.
     echo.
-    echo  Leave blank and Enter to go back.
 )
+if %try_invalid% == 1 (
+    echo  * Enter a valid number.
+    echo.
+)
+echo  * Enter R to reset to default value.
 echo.
-echo ------------------------------------------------------------
+echo ----------------------------------------------
 echo.
 set /p settings_try=No. of retry attempts: 
 if %try_invalid% == 0 if not defined settings_try goto :EOF
 if %try_invalid% == 1 if not defined settings_try goto settingsChangeDefinedTry
-if %try_invalid% == 1 if %settings_try% == r set settings_try=0
-if %try_invalid% == 1 if %settings_try% == R set settings_try=0
+if %settings_try% == r set settings_try=0
+if %settings_try% == R set settings_try=0
 echo "%settings_try%">"%aycdata%\try.txt"
 set /p defined_try=<"%aycdata%\try.txt"
 set defined_try=%defined_try:"=%
