dd56c5031661b7070c71c1dbfc78b593f6829244
Adithya S Sekhar <adithya0332@gmail.com>
2023-07-06 10:20:58 +0530

    settingsToggles: safer way to compare and change

--- scripts/settings/settingsToggles.bat
+++ scripts/settings/settingsToggles.bat
@@ -5,42 +5,42 @@ call :settings%settingsToggles.arg1%
 goto :EOF
 
 :settingsChangeAria2
-if %aria2_status% == 0 (
-    set aria2_status=1
+if %aria2_status% equ 0 (
+    set /a aria2_status=1
     echo "1">"%aycdata%\aria2_status.txt"
     goto :EOF
 )
-if %aria2_status% == 1 (
-    set aria2_status=2
+if %aria2_status% equ 1 (
+    set /a aria2_status=2
     echo "2">"%aycdata%\aria2_status.txt"
     goto :EOF
 )
-if %aria2_status% == 2 (
-    set aria2_status=0
+if %aria2_status% equ 2 (
+    set /a aria2_status=0
     echo "0">"%aycdata%\aria2_status.txt"
     goto :EOF
 )
 
 :settingsChangeThumbs
-if %thumbs_status% == 0 (
-    set thumbs_status=1
+if %thumbs_status% equ 0 (
+    set /a thumbs_status=1
     echo "1">"%aycdata%\thumbs_status.txt"
     goto :EOF
 )
-if %thumbs_status% == 1 (
-    set thumbs_status=0
+if %thumbs_status% equ 1 (
+    set /a thumbs_status=0
     echo "0">"%aycdata%\thumbs_status.txt"
     goto :EOF
 )
 
 :settingsChangeSubs
-if %subs_status% == 0 (
-    set subs_status=1
+if %subs_status% equ 0 (
+    set /a subs_status=1
     echo "1">"%aycdata%\subs_status.txt"
     goto :EOF
 )
-if %subs_status% == 1 (
-    set subs_status=0
+if %subs_status% equ 1 (
+    set /a subs_status=0
     echo "0">"%aycdata%\subs_status.txt"
     goto :EOF
 )
