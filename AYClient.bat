d12bd4d1f5dd77ab922ae10cd6e0b8a0c1cff231
Adithya S Sekhar <adithya0332@gmail.com>
2023-08-05 00:42:17 +0530

    set version 4.7 and update changelog

--- AYClient.bat
+++ AYClient.bat
@@ -9,7 +9,7 @@ set ayc.arg3=%3
 :: / Advanced Youtube Client - AYC Script                     /
 :: / Author          : Adithya S Sekhar                       /
 :: / First Release   : v1.0 (13/Aug/2016)                     /
-:: / Current Release : v4.6 (09/Jul/2023)                    /
+:: / Current Release : v4.7 (05/Aug/2023)                    /
 :: / Released under the MIT License.                          /
 :: / Please don't modify or redistribute without              /
 :: / proper credits.                                          /
