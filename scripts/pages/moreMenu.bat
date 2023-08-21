f883be4572bb32f8a4f5f65e324b97935d5dcde8
Adithya S Sekhar <adithya0332@gmail.com>
2023-08-21 20:29:17 +0530

    Add telegram channel link

--- scripts/pages/moreMenu.bat
+++ scripts/pages/moreMenu.bat
@@ -17,13 +17,16 @@ echo  (4) - Visit on GitHub
 echo.
 echo  (5) - Visit on Sourceforge
 echo.
+echo  (6) - Follow on Telegram
+echo.
 call tui borderSmallHalf
 echo.
-choice /c 012345 /n /m "Enter Choice (0-5): "
+choice /c 0123456 /n /m "Enter Choice (0-6): "
 if %errorlevel% == 1 goto :EOF
 if %errorlevel% == 2 start AYClient.bat "b"
 if %errorlevel% == 3 call settingsMenu
 if %errorlevel% == 4 call about
 if %errorlevel% == 5 start "" "https://github.com/adithya-s-sekhar/advanced-youtube-client-ayc"
 if %errorlevel% == 6 start "" "https://sourceforge.net/projects/advanced-youtube-client-ayc"
+if %errorlevel% == 7 start "" "https://t.me/ayc_news"
 goto moreMenu
