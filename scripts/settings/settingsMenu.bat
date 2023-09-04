e79083ed433b547c098fccbbcdc0817f1270ee13
Adithya S Sekhar <adithya0332@gmail.com>
2023-09-04 20:50:14 +0530

    Revert "move yt-dlp updater outside of settings"
    This reverts commit 0fe9e83293e41ac8a255040a486fb469444f9571.

--- scripts/settings/settingsMenu.bat
+++ scripts/settings/settingsMenu.bat
@@ -1,5 +1,5 @@
 :settingsMenu
-call tui windowSize %small_width% 31
+call tui windowSize %small_width% 34
 color %theme_colors%
 title AYC Settings
 cls
@@ -13,20 +13,23 @@ echo.
 echo  (2) - Change retry attempts
 echo        Currently: %defined_try%
 echo.
-echo  (3) - Multi-threaded downloading
+echo  (3) - Update yt-dlp (fixes most issues) - QuickKey: u
+echo        Currently: %youtube_dl_version%
+echo.
+echo  (4) - Multi-threaded downloading
 if %aria2_status% == 0 echo        [Disabled]
 if %aria2_status% == 1 echo        [Enabled for non-youtube downloads]
 if %aria2_status% == 2 echo        [Enabled for all downloads]
 echo.
-echo  (4) - Embed thumbnails
+echo  (5) - Embed thumbnails
 if %thumbs_status% == 0 echo        [Disabled]
 if %thumbs_status% == 1 echo        [Enabled]
 echo.
-echo  (5) - Embed subtitles
+echo  (6) - Embed subtitles
 if %subs_status% == 0 echo        [Disabled]
 if %subs_status% == 1 echo        [Enabled]
 echo.
-echo  (6) - Change theme - QuickKey: t
+echo  (7) - Change theme - QuickKey: t
 if %theme_status% == 0 echo        [Dark]
 if %theme_status% == 1 echo        [Light]
 if %theme_status% == 2 echo        [Blue]
@@ -34,17 +37,18 @@ if %theme_status% == 3 echo        [Purple]
 if %theme_status% == 4 echo        [Yellow]
 if %theme_status% == 5 echo        [Red]
 echo.
-echo  (7) - Reset AYC
+echo  (8) - Reset AYC
 echo.
 call tui borderSmallHalf
 echo.
-choice /c 01234567 /n /m "Select Option (0-7): "
+choice /c 012345678 /n /m "Select Option (0-8): "
 if %errorlevel% == 1 goto :EOF
 if %errorlevel% == 2 call settingsChangeDirHome
 if %errorlevel% == 3 call settingsChangeDefinedTry
-if %errorlevel% == 4 call settingsToggles ChangeAria2
-if %errorlevel% == 5 call settingsToggles ChangeThumbs
-if %errorlevel% == 6 call settingsToggles ChangeSubs
-if %errorlevel% == 7 call settingsToggles ChangeTheme
-if %errorlevel% == 8 call settingsReset
+if %errorlevel% == 4 call settingsYtUpdate
+if %errorlevel% == 5 call settingsToggles ChangeAria2
+if %errorlevel% == 6 call settingsToggles ChangeThumbs
+if %errorlevel% == 7 call settingsToggles ChangeSubs
+if %errorlevel% == 8 call settingsToggles ChangeTheme
+if %errorlevel% == 9 call settingsReset
 goto :settingsMenu
