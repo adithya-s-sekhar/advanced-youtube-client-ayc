85caa30f185058928aa9b275fe7b6f70e6ac4de7
Adithya S Sekhar <adithya0332@gmail.com>
2023-09-04 11:14:26 +0530

    settingsYtUpdate: basic version checking

--- scripts/settings/settingsYtUpdate.bat
+++ scripts/settings/settingsYtUpdate.bat
@@ -1,18 +1,22 @@
 set yt_updated=0
+set yt_no_update=0
 :update
-if %yt_updated% == 1 (
-    call tui windowSize %small_width% 22
-) else (
-    call tui windowSize %small_width% 20
-)
+set tmp_youtube_dl_version=0
+if %yt_updated% == 0 call tui windowSize %small_width% 20
+if %yt_updated% == 0 if %yt_no_update% == 1 call tui windowSize %small_width% 22
+if %yt_updated% == 1 call tui windowSize %small_width% 22
 color 0F
 title Update yt-dlp
 cls
 call tui bannerSmall
 echo.
-if %yt_updated% == 0 (
+if %yt_updated% == 0 if %yt_no_update% == 0 echo  Current yt-dlp version: %youtube_dl_version%
+if %yt_updated% == 0 if %yt_no_update% == 1 (
+    echo  No new version found. Try Nightly.
+    echo.
     echo  Current yt-dlp version: %youtube_dl_version%
-) else (
+)
+if %yt_updated% == 1 (
     echo  Updated successfully.
     echo.
     echo  Current yt-dlp version: %youtube_dl_version%
@@ -36,23 +40,29 @@ goto update
 
 
 :ytUpdateStable
+set yt_updated=0
 echo.
 echo Updating to latest stable
 echo.
 %youtube_dl% --update-to stable@2023.07.06 > nul
 %youtube_dl% --update-to stable > nul
 :: you can't downgrade from nightly without a specifig tag
+set tmp_youtube_dl_version=%youtube_dl_version%
 for /f %%i in ('yt-dlp --version') do set youtube_dl_version=%%i
 echo %youtube_dl_version%>"%aycdata%\youtube_dl_version.txt"
-set yt_updated=1
+if not %tmp_youtube_dl_version% == %youtube_dl_version% set yt_updated=1
+if %tmp_youtube_dl_version% == %youtube_dl_version% set yt_no_update=1
 goto :EOF
 
 :ytUpdateNightly
+set yt_updated=0
 echo.
 echo Updating to latest nightly
 echo.
 %youtube_dl% --update-to nightly  > nul
+set tmp_youtube_dl_version=%youtube_dl_version%
 for /f %%i in ('yt-dlp --version') do set youtube_dl_version=%%i
 echo %youtube_dl_version%>"%aycdata%\youtube_dl_version.txt"
-set yt_updated=1
+if not %tmp_youtube_dl_version% == %youtube_dl_version% set yt_updated=1
+if %tmp_youtube_dl_version% == %youtube_dl_version% set yt_no_update=1
 goto :EOF
