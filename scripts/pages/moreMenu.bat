4516acb2638d3a23b1fe4ea50bff524406320928
Adithya S Sekhar <adithya0332@gmail.com>
2023-09-04 20:50:26 +0530

    Revert "move links to about page"
    This reverts commit 3238226b544d220230cadf389565f65b2893df23.

--- scripts/pages/moreMenu.bat
+++ scripts/pages/moreMenu.bat
@@ -1,5 +1,5 @@
 :moreMenu
-call tui windowSize %small_width% 17
+call tui windowSize %small_width% 23
 color %theme_colors%
 title More Options
 cls
@@ -13,11 +13,20 @@ echo  (2) - Settings               - QuickKey: s
 echo.
 echo  (3) - About                  - QuickKey: a
 echo.
+echo  (4) - Visit on GitHub        - QuickKey: g
+echo.
+echo  (5) - Visit on Sourceforge   - QuickKey: sf
+echo.
+echo  (6) - Follow on Telegram
+echo.
 call tui borderSmallHalf
 echo.
-choice /c 0123 /n /m "Select Option (0-3): "
+choice /c 0123456 /n /m "Select Option (0-6): "
 if %errorlevel% == 1 goto :EOF
 if %errorlevel% == 2 start AYClient.bat "b"
 if %errorlevel% == 3 call settingsMenu
 if %errorlevel% == 4 call about
+if %errorlevel% == 5 start "" "https://github.com/adithya-s-sekhar/advanced-youtube-client-ayc"
+if %errorlevel% == 6 start "" "https://sourceforge.net/projects/advanced-youtube-client-ayc"
+if %errorlevel% == 7 start "" "https://t.me/ayc_news"
 goto moreMenu
