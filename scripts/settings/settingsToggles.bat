2769395460663cba867a729d7b25676027629b59
Adithya S Sekhar <adithya0332@gmail.com>
2023-09-05 13:40:02 +0530

    settingsToggles: refactor

--- scripts/settings/settingsToggles.bat
+++ scripts/settings/settingsToggles.bat
@@ -1,96 +1,98 @@
 set settingsToggles.arg1=%~1
 
-call :settings%settingsToggles.arg1%
+call :toggle%settingsToggles.arg1%
+
+call :save%settingsToggles.arg1%
 
 goto :EOF
 
-:settingsChangeAria2
+:toggleChangeAria2
 if %aria2_status% == 0 (
     set aria2_status=1
-    echo "1">"%aycdata%\aria2_status.txt"
     goto :EOF
 )
 if %aria2_status% == 1 (
     set aria2_status=2
-    echo "2">"%aycdata%\aria2_status.txt"
     goto :EOF
 )
 if %aria2_status% == 2 (
     set aria2_status=0
-    echo "0">"%aycdata%\aria2_status.txt"
     goto :EOF
 )
 
-:settingsChangeThumbs
+:toggleChangeThumbs
 if %thumbs_status% == 0 (
     set thumbs_status=1
-    echo "1">"%aycdata%\thumbs_status.txt"
     goto :EOF
 )
 if %thumbs_status% == 1 (
     set thumbs_status=0
-    echo "0">"%aycdata%\thumbs_status.txt"
     goto :EOF
 )
 
-:settingsChangeSubs
+:toggleChangeSubs
 if %subs_status% == 0 (
     set subs_status=1
-    echo "1">"%aycdata%\subs_status.txt"
     goto :EOF
 )
 if %subs_status% == 1 (
     set subs_status=0
-    echo "0">"%aycdata%\subs_status.txt"
     goto :EOF
 )
 
-:settingsChangeTheme
+:toggleChangeTheme
 if %theme_status% == 0 (
     set theme_status=1
-    echo "1">"%aycdata%\theme_status.txt"
-    call themer
     goto :EOF
 )
 if %theme_status% == 1 (
     set theme_status=2
-    echo "2">"%aycdata%\theme_status.txt"
-    call themer
     goto :EOF
 )
 if %theme_status% == 2 (
     set theme_status=3
-    echo "3">"%aycdata%\theme_status.txt"
-    call themer
     goto :EOF
 )
 if %theme_status% == 3 (
     set theme_status=4
-    echo "4">"%aycdata%\theme_status.txt"
-    call themer
     goto :EOF
 )
 if %theme_status% == 4 (
     set theme_status=5
-    echo "5">"%aycdata%\theme_status.txt"
-    call themer
     goto :EOF
 )
 if %theme_status% == 5 (
     set theme_status=0
-    echo "0">"%aycdata%\theme_status.txt"
-    call themer
     goto :EOF
 )
 
-:settingsYtUpdateOnStart
+:toggleYtUpdateOnStart
 if %ytupd_onstart% == 0 (
     set ytupd_onstart=1
-    echo "1">"%aycdata%\ytupd_onstart.txt"
     goto :EOF
 )
 if %ytupd_onstart% == 1 (
     set ytupd_onstart=0
-    echo "0">"%aycdata%\ytupd_onstart.txt"
     goto :EOF
 )
+
+:saveChangeAria2
+echo "%aria2_status%">"%aycdata%\aria2_status.txt"
+goto :EOF
+
+:saveChangeThumbs
+echo "%thumbs_status%">"%aycdata%\thumbs_status.txt"
+goto :EOF
+
+:saveChangeSubs
+echo "%subs_status%">"%aycdata%\subs_status.txt"
+goto :EOF
+
+:saveChangeTheme
+call themer
+echo "%theme_status%">"%aycdata%\theme_status.txt"
+goto :EOF
+
+:saveYtUpdateOnStart
+echo "%ytupd_onstart%">"%aycdata%\ytupd_onstart.txt"
+goto :EOF
