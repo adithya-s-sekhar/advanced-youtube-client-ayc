c24b86d54036f44ebc7bb20f657faf9272c778f5
Adithya S Sekhar <adithya0332@gmail.com>
2023-09-24 07:50:55 +0530

    siteFixes: enable reddit thumbnails fixed in 2023.09.24

--- scripts/lib/siteFixes.bat
+++ scripts/lib/siteFixes.bat
@@ -1,10 +1,11 @@
 set siteFixes.arg1=%~1
 
-echo %siteFixes.arg1% | findstr /i /r /c:"^https://www.reddit.com" /c:"^https://reddit.com" /c:"^http://www.reddit.com" /c:"^http://reddit.com" /c:"^http://redd.it" /c:"^https://redd.it" /c:"^http://v.redd.it" /c:"^https://v.redd.it" > nul
-if %errorlevel% == 0 call :reddit && goto :EOF
+:: Reddit thumbnails fixed in 2023.09.24
+@REM echo %siteFixes.arg1% | findstr /i /r /c:"^https://www.reddit.com" /c:"^https://reddit.com" /c:"^http://www.reddit.com" /c:"^http://reddit.com" /c:"^http://redd.it" /c:"^https://redd.it" /c:"^http://v.redd.it" /c:"^https://v.redd.it" > nul
+@REM if %errorlevel% == 0 call :reddit && goto :EOF
 
 goto :EOF
 
-:reddit
-set "thumbs="
-goto :EOF
+@REM :reddit
+@REM set "thumbs="
+@REM goto :EOF
