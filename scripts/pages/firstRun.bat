92678c2a4aff42ba450f6456384f21642cd50cc5
Adithya S Sekhar <adithya0332@gmail.com>
2023-10-02 11:08:31 +0530

    firstRun: update messaging on long path

--- scripts/pages/firstRun.bat
+++ scripts/pages/firstRun.bat
@@ -100,12 +100,12 @@ cls
 echo.
 echo  (2/4) Enable support for long path names
 echo.
-echo  - Windows has a file name or path limit of 260 characters. 
+echo  - Windows has a file name + path limit of 260 characters. 
 echo.
 echo  - We can increase the limit to 32,767 characters to support videos with large file names.
 echo.
-echo  - If you skip this, videos with a file name longer than 260 characters will fail to 
-echo    download.
+echo  - If you skip this, videos with a file name with path longer than 260 characters will 
+echo    fail to download.
 echo.
 call tui borderMediumHalf
 echo.
