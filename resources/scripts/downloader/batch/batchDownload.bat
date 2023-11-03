ee0605c95f061812683e033dc0a32105544ad20d
Adithya S Sekhar <adithya0332@gmail.com>
2023-11-03 16:20:37 +0530

    batch youtube: call new sitefix

--- resources/scripts/downloader/batch/batchDownload.bat
+++ resources/scripts/downloader/batch/batchDownload.bat
@@ -20,7 +20,7 @@ set try=1
 set conf=%conf:"=%
 if %aria2_status% == 1 if not %job_type% == 1 set aria2=--external-downloader aria2c
 if %aria2_status% == 1 if %job_type% == 1 set aria2=--concurrent-fragments 8
-call siteFixes "%url%"
+if %job_type% == 1 call siteFixes "youtube"
 
 :batchDownloadTried
 set error_mode=batch
