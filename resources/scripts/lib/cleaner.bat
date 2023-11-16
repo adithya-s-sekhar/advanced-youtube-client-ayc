d68a72c6e889c0a109f2b247eaf57e5231796e65
Adithya S Sekhar <adithya0332@gmail.com>
2023-11-16 11:58:26 +0530

    introduce cleaner

---
+++ resources/scripts/lib/cleaner.bat
@@ -0,0 +1,53 @@
+:: Advanced Youtube Client - AYC
+:: Copyright (C) 2023  Adithya S Sekhar
+::
+:: This program is free software: you can redistribute it and/or modify
+:: it under the terms of the GNU General Public License as published by
+:: the Free Software Foundation, either version 3 of the License, or
+:: (at your option) any later version.
+::
+:: This program is distributed in the hope that it will be useful,
+:: but WITHOUT ANY WARRANTY; without even the implied warranty of
+:: MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+:: GNU General Public License for more details.
+::
+:: You should have received a copy of the GNU General Public License
+:: along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+set cleaner.arg1=%~1
+
+if "%cleaner.arg1%" == "sq" call :cleanSingleQuote & goto :EOF
+if "%cleaner.arg1%" == "dq" call :cleanDoubleQuote & goto :EOF
+if "%cleaner.arg1%" == "w" call :cleanWhitespace & goto :EOF
+if "%cleaner.arg1%" == "lw" call :cleanLeadingWhitespace & goto :EOF
+if "%cleaner.arg1%" == "tw" call :cleanTrailingWhitespace & goto :EOF
+
+goto :EOF
+
+:cleanSingleQuote
+set input=%input:'=%
+goto :EOF
+
+:cleanDoubleQuote
+set input=%input:"=%
+goto :EOF
+
+:cleanWhitespace
+set input=%input: =%
+goto :EOF
+
+:cleanLeadingWhitespace
+:cleanLeadingWhitespaceCheck
+if "%input:~0,1%"==" " goto cleanLeadingWhitespaceClean
+goto :EOF
+:cleanLeadingWhitespaceClean
+set "input=%input:~1%"
+goto cleanLeadingWhitespaceCheck
+
+:cleanTrailingWhitespace
+:cleanTrailingWhitespaceCheck
+if "%input:~-1%"==" " goto cleanTrailingWhitespaceClean
+goto :EOF
+:cleanTrailingWhitespaceClean
+set "input=%input:~0,-1%"
+goto cleanTrailingWhitespaceCheck
