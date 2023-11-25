d80562e2629c8e1da6e862c5c63e0fad44f341be
Adithya S Sekhar <adithya0332@gmail.com>
2023-11-26 00:16:45 +0530

    linkValidator: support links that don't begin with http://

--- resources/scripts/lib/linkValidator.bat
+++ resources/scripts/lib/linkValidator.bat
@@ -16,28 +16,26 @@
 
 set linkValidator.arg1=%~1
 
-set link_validator=0
+set link_validator=1
 set youtube_link=0
 set bilibili_link=0
 set twitch_link=0
 set generic_link=0
 
-echo %linkValidator.arg1% | findstr /i /r /c:"^http://" /c:"^https://" > nul
-if %errorlevel% == 0 (
-    set link_validator=1
-) else (
-    goto :EOF
-)
-
-echo %linkValidator.arg1% | findstr /i /r /c:"^https://www.youtube.com" /c:"^https://m.youtube.com" /c:"^https://youtube.com" /c:"^https://youtu.be" /c:"^http://www.youtube.com" /c:"^http://m.youtube.com" /c:"^http://youtube.com" /c:"^http://youtu.be" > nul
+echo %linkValidator.arg1% | findstr /i /r /c:"^https://www.youtube.com" /c:"^https://m.youtube.com" /c:"^https://youtube.com" /c:"^https://youtu.be" /c:"^http://www.youtube.com" /c:"^http://m.youtube.com" /c:"^http://youtube.com" /c:"^http://youtu.be" /c:"^www.youtube.com" /c:"^m.youtube.com" /c:"^youtube.com" /c:"^youtu.be" > nul
 if %errorlevel% == 0 set youtube_link=1 & goto :EOF
 
-echo %linkValidator.arg1% | findstr /i /r /c:"^https://www.bilibili.com" /c:"^https://m.bilibili.com" /c:"^https://bilibili.com" /c:"^http://www.bilibili.com" /c:"^http://m.bilibili.com" /c:"^http://bilibili.com" /c:"^https://www.bilibili.tv" /c:"^https://bilibili.tv" /c:"^http://www.bilibili.tv" /c:"^http://bilibili.tv" > nul
+echo %linkValidator.arg1% | findstr /i /r /c:"^https://www.bilibili.com" /c:"^https://m.bilibili.com" /c:"^https://bilibili.com" /c:"^http://www.bilibili.com" /c:"^http://m.bilibili.com" /c:"^http://bilibili.com" /c:"^https://www.bilibili.tv" /c:"^https://bilibili.tv" /c:"^http://www.bilibili.tv" /c:"^http://bilibili.tv" /c:"^www.bilibili.com" /c:"^bilibili.com" /c:"^www.bilibili.tv" /c:"^bilibili.tv" > nul
 if %errorlevel% == 0 set bilibili_link=1 & goto :EOF
 
-echo %linkValidator.arg1% | findstr /i /r /c:"^https://www.twitch.tv" /c:"^https://twitch.tv" /c:"^http://www.twitch.tv" /c:"^http://twitch.tv" > nul
+echo %linkValidator.arg1% | findstr /i /r /c:"^https://www.twitch.tv" /c:"^https://twitch.tv" /c:"^http://www.twitch.tv" /c:"^http://twitch.tv" /c:"^www.twitch.tv" /c:"^twitch.tv" > nul
 if %errorlevel% == 0 set twitch_link=1 & goto :EOF
 
-set generic_link=1
-
-goto :EOF
+echo %linkValidator.arg1% | findstr /i /r /c:"^http://" /c:"^https://" > nul
+if %errorlevel% == 0 (
+    set generic_link=1
+    goto :EOF
+) else (
+    set link_validator=0
+    goto :EOF
+)
